5fbf9aaa4f258dbd39e06183aa080f31f9e8ba0a
Hellmann Arias <92462240+HellmannArias@users.noreply.github.com>
2023-01-03 13:33:06 -0500

    Add files via upload

---
+++ Exercises.js
@@ -0,0 +1,5 @@
+let fergei = "Corriendo rapido hacia atras";
+console.log(fergei.slice(23, 28).length);
+
+let volvo = "Hablando chino Mandarin";
+console.log(volvo.slice(9, 14).length);
---
+++ Exercises2.js
@@ -0,0 +1,12 @@
+const user = {
+    name: 'Erik',
+    description: 'Me gusta la programación'
+};
+
+const user2 = {
+    name: 'Daniel',
+    description: 'Estoy aprendiendo a programar'
+};
+
+console.log(user.description.slice(0, 2).length);
+console.log(user2.description.slice(0, 5).length);
